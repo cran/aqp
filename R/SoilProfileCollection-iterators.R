3727c188ec3b3848f8bb92891329087548ee60b0
Dylan Beaudette <debeaudette@ucdavis.edu>
2011-01-18 00:00:00 +0000

    version 0.99-9.51

--- R/SoilProfileCollection-iterators.R
+++ R/SoilProfileCollection-iterators.R
@@ -1,26 +1,48 @@
 
-# analog to apply():
-# operates along horizons, by profile
-# returning n = nrow(horizons(obj))
+# split an SPC into a list of SPCs, each with 1 profile
+if (!isGeneric("splitProfiles"))
+ 	setGeneric("splitProfiles", function(object) standardGeneric("splitProfiles"))
 
-## TODO: how can we get access the to the site data for profile_i ?
-
-# returns a data.frame
-if (!isGeneric("profileApply"))
-  setGeneric("profileApply", function(object, FUN, ...) standardGeneric("profileApply"))
-
-
-setMethod(f='profileApply', signature='SoilProfileCollection',
-  function(object, FUN, ...){
-	
-	h <- horizons(object)
-	l <- dlply(h, idname(object), .fun=FUN, ...)
-	res <- unlist(l)
-	
-	return(res)
-  }
+setMethod(f='splitProfiles', signature='SoilProfileCollection',
+	function(object) {
+						
+		# get profile IDs
+		pIDs <- profile_id(object)
+					
+		# init empty list
+		l <- list()
+						
+		# iterate over SPC, spliting into a list of SPC_i ... SPC_n
+		for(i in seq_along(pIDs)) {
+			pID <- pIDs[i]
+			l[[pID]] <- object[i, ]
+		}
+						
+		return(l)
+	}
 )
 
+
+
+# analog to apply():
+# operates by profile, returns vector of length(object) or nrow(object)
+
+if (!isGeneric("profileApply"))
+	setGeneric("profileApply", function(object, FUN, ...) standardGeneric("profileApply"))
+
+setMethod(f='profileApply', signature='SoilProfileCollection',
+	function(object, FUN, ...) {
+		
+		# split into a list of 1-profile SPC objects
+		s <- splitProfiles(object)
+		
+		# apply function by SPC_i
+		l <- llply(s, .fun=FUN, ...)
+		
+		# return as vector
+		return(unlist(l))
+	}
+)
 
 
 
